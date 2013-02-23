127336db0396c2a2715c763a3e5bfcdada45886d
Bibhas Debnath <bivunlim@gmail.com>
2013-02-24 04:21:26 +0530

    added readme instructions

---
+++ README.md
@@ -0,0 +1,23 @@
+gitinit
+=======
+
+Initiates an empty git repository with a `.gitignore` file for the provided language.
+
+Installation
+------------
+
+    $ [sudo] pip install gitinit
+
+Usage
+-----
+    
+    # initiates with a generic .gitignore file
+    $ gitinit
+
+    # initiates a Haskell .gitignore file
+    $ gitignore -l haskell
+
+Help
+----
+
+    $ gitinit -h
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     keywords = "example documentation tutorial",
     url = "https://github.com/iambibhas/gitinit",
     packages=find_packages(),
-    long_description=read('README.txt'),
+    long_description=read('README.md'),
     package_data={'gitinit': ['*.gitignore', 'gitinit/gitignores/*.gitignore', 'gitinit/gitignores/Global/*.gitignore']},
     include_package_data=True,
     entry_points={"console_scripts": ["gitinit=gitinit.gitinit:main"]},
