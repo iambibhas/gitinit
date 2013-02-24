00df80d3b1cfa61b7b8167d588afa945924c3061
Bibhas Debnath <bivunlim@gmail.com>
2013-02-24 14:16:24 +0530

    fixed manifest

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import os
 from setuptools import setup, find_packages
 
+__currdir__ = os.getcwd()
+__readme__ = os.path.join(__currdir__, 'README')
+
 setup(
     name="gitinit",
-    version="1.0.2",
+    version="1.0.4",
     author="Bibhas C Debnath",
     author_email="me@bibhas.in",
     description=("Initiates git with gitignore for provided language"),
@@ -11,7 +14,7 @@ setup(
     keywords="cli git",
     url="https://github.com/iambibhas/gitinit",
     packages=find_packages(),
-    long_description=open('README.md').read(),
+    long_description=open(__readme__).read(),
     package_data={'gitinit': ['*.gitignore', 'gitinit/gitignores/*.gitignore', 'gitinit/gitignores/Global/*.gitignore']},
     include_package_data=True,
     entry_points={"console_scripts": ["gitinit=gitinit.gitinit:main"]},
